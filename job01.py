4d875f6a9802d33e42a298aea21355bb7c93d787
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-05-10 10:12:31 +0200

    dernier enregistrement

--- job01.py
+++ job01.py
@@ -13,6 +13,22 @@ l'utilisateur de choisir un nouveau mot de passe.
 5. Répétez les étapes 2 à 4 jusqu'à ce que l'utilisateur choisisse un mot de passe
 valide.
 utilisez sha256() pour créer un objet de calcul d'empreinte avec l'algorithme SHA-256."""
+"""● Il doit contenir au moins 8 caractères.
+● Il doit contenir au moins une lettre majuscule.
+● Il doit contenir au moins une lettre minuscule.
+● Il doit contenir au moins un chiffre.
+● Il doit contenir au moins un caractère spécial (!, @, #, $, %, ^, &, *).
+● Voici comment votre programme devrait fonctionner :
+1. Demandez à l'utilisateur de choisir un mot de passe.
+2. Vérifiez si le mot de passe choisi respecte les exigences de sécurité.
+3. Si le mot de passe est valide, affichez un message de confirmation et quittez le
+programme.
+4. Si le mot de passe n'est pas valide, affichez un message d'erreur et demandez à
+l'utilisateur de choisir un nouveau mot de passe.
+5. Répétez les étapes 2 à 4 jusqu'à ce que l'utilisateur choisisse un mot de passe
+valide.
+utilisez sha256() pour créer un objet de calcul d'empreinte avec l'algorithme SHA-256."""
+
 import hashlib
 
 def my_password():
@@ -41,3 +57,4 @@ def sha256(string):
     return hash_object.hexdigest()
 
 my_password()
+
--- job03.py
+++ job03.py
@@ -1,22 +1,69 @@
-"""Comparer les mots de passe afin de ne pas avoir 2 fois le même mot de passe
-enregistré dans le fichier."""
-
 import json
+import os
+import random
+import hashlib
 
-with open('job02_my_passwords.json') as f:
-    data = json.load(f)
+# Définir le nom du fichier JSON
+file_name = 'job02_my_passwords.json'
 
-my_chaine = []
-for i in data:
-    allPassword = data[i]['my_password']
-    if allPassword in my_chaine:
-        print(allPassword , "two equals passwords!")
-        break
-    else:
-        my_chaine.append(allPassword)
+# Vérifier si le fichier existe et le charger
+if os.path.isfile(file_name):
+    with open(file_name, 'r') as file:
+        passwords = json.load(file)
 else:
-    print("no repet object")
+    passwords = {}
 
+# Demander les informations pour un nouveau mot de passe
+new_password = {}
+new_password['name'] = input("Nom du compte: ")
+new_password['username'] = input("Nom d'utilisateur: ")
 
+def generate_password(characters):
+    print('Longueur du mot de passe: ')
+    password_length = int(input())
+    password = ''
+    for i in range(password_length):
+        password += random.choice(characters)
+    print('Mot de passe généré:', password)
+    hashed_password = sha256(password)
+    print('Le mot de passe haché est:', hashed_password)
+    return password, hashed_password
 
-        
+def sha256(password_hashed):
+    hash_object = hashlib.sha256(password_hashed.encode('utf-8'))
+    return hash_object.hexdigest()
+
+# Définir les caractères pour la génération de mot de passe
+characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{}|;:<>,.?/~`'
+
+password, hashed_password = generate_password(characters)
+
+# Vérifier si le mot de passe existe déjà dans le fichier
+hashed_passwords = [p['my_password'] for p in passwords.values()]
+if hashed_password in hashed_passwords:
+    print("Le mot de passe existe déjà. Veuillez en générer un nouveau.")
+else:
+    new_password['my_password'] = hashed_password
+    # Ajouter la nouvelle entrée de mot de passe
+    passwords[new_password['name']] = new_password
+    # Écrire les données dans le fichier JSON
+    with open(file_name, 'w') as file:
+        json.dump(passwords, file, indent=4)
+
+# Afficher les mots de passe
+if passwords:
+    for password in passwords.values():
+        print(f"Nom du compte: {password['name']}")
+        print(f"Nom d'utilisateur: {password['username']}")
+        print(f"Mot de passe: {password['my_password']}\n")
+else:
+    print("Aucun mot de passe enregistré.")
+
+# Vérifier si des mots de passe sont en double
+hashed_passwords = [p['my_password'] for p in passwords.values()]
+duplicates = set(x for x in hashed_passwords if hashed_passwords.count(x) > 1)
+if duplicates:
+    print(f"Les mots de passe en double sont: {duplicates}")
+else:
+    print("Aucun mot de passe en double.")
+
